56a411993e8371194b1cbc257fe0ec7ad1880f75
Aurelie Mendes <aurelie.mds@outlook.fr>
2024-04-09 16:10:30 +0200

    removing lombok

--- src/main/java/com/formation/entity/User.java
+++ src/main/java/com/formation/entity/User.java
@@ -7,15 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Entity
 @Table(name = "users")
 
@@ -29,4 +22,42 @@ public class User {
 	private String lastName;
 	@Column(nullable = false, unique = true)
 	private String email;
+	
+	public User() {
+		
+	}
+	
+	public User( String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 }
